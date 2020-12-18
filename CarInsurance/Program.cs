e8775cdea86eb3f5b8124cb60f54f6070b8e7a79
TristanHillyard <73565670+TristanHillyard@users.noreply.github.com>
2020-12-18 10:03:21 -0700

    Add project files.

---
+++ CarInsurance/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CarInsurance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Have you ever had a DUI?");
+            bool DUI = Convert.ToBoolean(Console.ReadLine());
+            if(DUI == true)
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+
+            if(age > 15 && DUI == false && tickets <= 3)
+            {
+                Console.WriteLine("Qualified");
+            }
+            else
+            {
+                Console.WriteLine("Not Qualified");
+            }
+
+        }
+    }
+}
